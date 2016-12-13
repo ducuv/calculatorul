07ebc13205a19f5a1ef1e2f2ac767fddfa7fb1d5
Filipescu <I329635@global.corp.sap>
2016-12-13 22:53:57 +0200

    apasa butoanele

--- public/scripts/testing.js
+++ public/scripts/testing.js
@@ -4,13 +4,32 @@ function sum(a,b){
 	var a = document.getElementById('a').value;
 	var b = document.getElementById('b').value;
 	var sum = parseInt(a)+parseInt(b);
-	document.getElementById('sum').value = sum;
+	document.getElementById('result').value = sum;
 	return sum;
 }
 
-function numbers(x)
-{
-		document.getElementById('result').value = x;
+var array = [];
+var number="";
+
+function numbers(x){
+
+		//number = number + x;
+		// array.concat(x+array);
+		// x=array + x;
+	 	//    array.push(x);
+		//	var length = array.length;
+		//	for(var i=0; i<length;i++)
+		//	array.concat(x+array[i]);
+
+		number= x+number;
+		parseInt(number);
+		document.getElementById('result').value = number;
+		array.push(number);
+		return x;
+	};
+
+function sign(){
+
+		document.getElementById('result').value = number + "+";
 	
-	return x;
 }
